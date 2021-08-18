eb33ff28d543c2fc4a3cc363d5d4f00e29458324
mingkyy <alsrus1227@naver.com>
2021-08-18 14:49:49 +0900

    AntTest 코드

---
+++ src/test/java/com/megait/mymall/AntTests.java
@@ -0,0 +1,70 @@
+package com.megait.mymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.AntPathMatcher;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AntTests {
+    @Test
+    public void AntTests() {
+        // double asterisks
+        assertThat(checkAntPattern("/static/**", "/static/a.jpg")).isTrue();
+        assertThat(checkAntPattern("/static/**", "/static/css/a.css")).isTrue();
+        assertThat(checkAntPattern("/static/**", "/x/static/css/a.css")).isFalse();
+
+//
+//        assertThat(true, is(checkAntPattern("/static/**", "/static/a.jpg")));
+//        assertThat(true, is(checkAntPattern("/static/**", "/static/css/a.css")));
+//        assertThat(true, is(checkAntPattern("/static/**", "/static/js/a.js")));
+//        assertThat(true, is(checkAntPattern("/static/**", "/static/img/a.jpg")));
+//        assertThat(true, is(checkAntPattern("/static/**", "/static/a/b/c/d/e/f/g/a.jpg")));
+//        assertThat(true, is(checkAntPattern("/static/**", "/static")));
+//        assertThat(true, is(checkAntPattern("/static/**", "/static/")));
+//
+//        // single asterisks
+//        assertThat(true, is(checkAntPattern("/static/*", "/static/a.jpg")));
+//        assertThat(true, is(checkAntPattern("/static/*", "/static/namkyuProfilePicture.jpg")));
+//
+//        assertThat(false, is(checkAntPattern("/static/*", "/static/a/test.jpg")));
+//        assertThat(false, is(checkAntPattern("/static/*", "/static/a/b/c/d/test.jpg")));
+//
+//        assertThat(true, is(checkAntPattern("/static*/*", "/static/test.jpg")));
+//        assertThat(true, is(checkAntPattern("/static*/*", "/static1/test.jpg")));
+//        assertThat(true, is(checkAntPattern("/static*/*", "/static123/test.jpg")));
+//        assertThat(true, is(checkAntPattern("/static*/*", "/static-123/test.jpg")));
+//        assertThat(true, is(checkAntPattern("/static*/*", "/static~!@#$%^&*()_+}{|/test.jpg")));
+//
+//        assertThat(false, is(checkAntPattern("/static*/*", "/static12/a/test.jpg")));
+//        assertThat(false, is(checkAntPattern("/static*/*", "/static12/a/b/test.jpg")));
+//
+//        // double and single combine
+//        assertThat(true, is(checkAntPattern("/static*/**", "/static/a.jpg")));
+//        assertThat(true, is(checkAntPattern("/static*/**", "/static1/a.jpg")));
+//        assertThat(true, is(checkAntPattern("/static*/**", "/static/a/a.jpg")));
+//        assertThat(true, is(checkAntPattern("/static*/**", "/static/a/b/a.jpg")));
+//        assertThat(true, is(checkAntPattern("/static*/**", "/static/a/b/c/a.jpg")));
+//
+//        assertThat(true, is(checkAntPattern("**/static/**", "a/static/a/b/c/a.jpg")));
+//        assertThat(true, is(checkAntPattern("**/static/**", "a/b/static/a/b/c/a.jpg")));
+//
+//
+//        // question-mark
+//        assertThat(true, is(checkAntPattern("/static-?/**", "/static-a/a.jpg")));
+//        assertThat(true, is(checkAntPattern("/static-?/**", "/static-a/b/c/a.jpg")));
+//        assertThat(true, is(checkAntPattern("/static-?/*", "/static-a/abcd.jpg")));
+//        assertThat(true, is(checkAntPattern("/static-?/???.jpg", "/static-a/abc.jpg")));
+
+    }
+
+    private boolean checkAntPattern(String pattern, String inputStr) {
+        return matches(pattern, inputStr);
+    }
+
+    public static boolean matches(String pattern, String inputStr) {
+        AntPathMatcher antPathMatcher = new AntPathMatcher();
+        return antPathMatcher.match(pattern, inputStr);
+    }
+}
+
